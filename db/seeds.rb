8974fa78f62287c8930bacc66409a896c421ddc2
Jean Motte <jijimotte@gmail.com>
2024-07-19 12:03:45 +0200

    retrieved some info to constitute book instance from the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,19 @@ def api_key
 end
 
 google_id = '102466684172017991017'
-url = "https://www.googleapis.com/books/v1/users/#{google_id}/bookshelves/4?key=#{api_key}"
+url = "https://www.googleapis.com/books/v1/users/#{google_id}/bookshelves/4/volumes?key=#{api_key}"
 
 response = RestClient.get(url)
 data = JSON.parse(response.body)
 
-puts data
+# Check if the response contains items
+if data["items"]
+  data["items"].each do |item|
+    title = item["volumeInfo"]["title"]
+    author = item["volumeInfo"]["authors"][0]
+
+    puts "Title: #{title}\nAuthors: #{author}"
+  end
+else
+  puts "No books found in the shelf."
+end
